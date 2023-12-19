a944b0294b53540323917d73eae25113933a44a3
Bigger, Thomas <thomasbigger584@gmail.com>
2023-12-19 18:04:56 +0000

    deleting any files before creating

--- record.sh
+++ record.sh
@@ -5,28 +5,46 @@ client_name="ARIUS"
 output_file="output"
 
 get_midi_port() {
-    device_output=$(arecordmidi -l)
-    local port;
-    port=$(echo "$device_output" | grep "$client_name" | awk '{print $1}')
-    if [ -n "$port" ]; then
-        echo "$port"
-    else
-        echo "No MIDI client found with the name: $client_name"
-        exit 1
-    fi
+  device_output=$(arecordmidi -l)
+  local port;
+  port=$(echo "$device_output" | grep "$client_name" | awk '{print $1}')
+  if [ -n "$port" ]; then
+      echo "$port"
+  else
+      echo "No MIDI client found with the name: $client_name"
+      exit 1
+  fi
+}
+
+start_recording() {
+  local mid_file
+  mid_file="$output_file.mid"
+  if [ -e "$mid_file" ]; then
+      rm "$mid_file"
+  fi
+  echo "Recording..."
+  arecordmidi --port "$1" "$mid_file"
 }
 
 convert_midi_to_mp3() {
+  local mp3_file
+  mp3_file="$output_file.mp3"
+  if [ -e "$mp3_file" ]; then
+      rm "$mp3_file"
+  fi
   echo "Converting MIDI to MP3..."
-  timidity "$output_file.mid" -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k "$output_file.mp3"
-  mpg123 "$output_file.mp3"
-  exit 0
+  timidity "$output_file.mid" -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k "$mp3_file"
+  play "$mp3_file"
 }
 trap convert_midi_to_mp3 INT
 
-main() {
-  midi_port=$(get_midi_port)
-  arecordmidi --port "$midi_port" "$output_file.mid"
+play() {
+  echo "Playing $1..."
+  mpg123 "$1"
 }
 
-main
+midi_port=$(get_midi_port)
+start_recording "$midi_port"
+
+echo "Done"
+exit 0
